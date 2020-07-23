253fcc0c42894b890e3631cd7456d300cafd3556
AlexeyGorbachevskiy <alexeygorbachevskiyy@gmail.com>
2020-07-23 20:26:20 +0300

    React.memo was added to the project

---
+++ src/stories/ReactMemo.stories.tsx
@@ -0,0 +1,61 @@
+import React, {useState} from 'react';
+
+
+
+export default {
+    title: 'ReactMemo',
+};
+
+
+const NewMessagesCounter = (props: any) => {
+    return <div>{props.count}</div>
+}
+const SecretUsers = (props: { users: Array<string> }) => {
+    console.log('Users Secret')
+    return <div>{props.users.map((u, i) => <div key={i}>{u}</div>)}</div>
+}
+const Users = React.memo(SecretUsers)
+
+export const ReactMemoDemo = () => {
+    const [counter, setCounter] = useState(0);
+    const [users, setUsers] = useState(['Alex', 'Bob', 'Boris']);
+    return (
+        <>
+            <button onClick={() => {
+                setCounter(counter + 1)
+            }}>+
+            </button>
+            <button onClick={() => {
+                setUsers([...users, 'Valentin'])
+            }}>Add Users
+            </button>
+            <NewMessagesCounter count={counter}/>
+            <Users users={users}/>
+            {}
+        </>
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
